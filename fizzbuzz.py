7af9f1e6e0e714d558c806b6121e22afe567dd97
marina <you@example.com>
2022-08-23 18:41:10 +0300

    change

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -7,6 +7,8 @@
 # 1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, Fizz Buzz, 16, 17, 
 # Fizz, 19, Buzz, Fizz, 22, 23, Fizz, Buzz, 26, Fizz, 28, 29, Fizz Buzz, 31, 32, Fizz, 34, Buzz, Fizz, ...
 
+
+# for loop
 def fizz_buzz():
     result_list = []
     for number in range(1,101):
@@ -18,6 +20,29 @@ def fizz_buzz():
             result_list.append("Buzz")
         else:
             result_list.append(number) 
+    return result_list
+
+print(fizz_buzz())
+
+
+print()
+print()
+print()
+
+# while loop
+def fizz_buzz():
+    number = 1
+    result_list = []
+    while number <= 100:
+        if (number % 3 == 0) and (number % 5 == 0):
+            result_list.append("Fizz Buzz")
+        elif (number % 3 == 0):
+            result_list.append("Fizz")
+        elif (number % 5 == 0) and (number % 5 == 0):
+            result_list.append("Buzz")
+        else:
+            result_list.append(number) 
+        number += 1
     return result_list
 
 
